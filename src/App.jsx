4493d3ccc60ac603ecd185a6d441f57bd0460dbe
Enesi Wise <enesiwise@gmail.com>
2023-08-30 12:11:38 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import HomePage from "./components/HomePage";
 import QuizPage from "./components/QuizPage";
 import blobUp from "./images/blobUp.png";
 import blobDown from "./images/blobDown.png";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div>
       <img className="blob-up" src={blobUp} alt="yellow-blob" />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="Quiz" element={<QuizPage />}></Route>
-        </Routes>
-      </BrowserRouter>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="Quiz" element={<QuizPage />}></Route>
+      </Routes>
+
       <img className="blob-down" src={blobDown} alt="blue-blob" />
     </div>
   );
